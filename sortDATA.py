48aeab114fbe1b7cadb7b71340941ccb6fd2b5e5
DrRico <851774781@qq.com>
2019-12-31 10:50:43 +0800

    add .mat

--- sortDATA.py
+++ sortDATA.py
@@ -17,7 +17,7 @@ with open("keysFormat.txt","rb") as f:
 for i,j in zip(loc,rss):
     #print(i,j)
     pass
-scio.savemat('filename.mat', mdict={"loc":loc})
+#scio.savemat('filename.mat', mdict={"loc":loc})
 
 with open("keysFormat.txt","rb") as f:
     macKeys = pickle.load(f)
@@ -29,14 +29,20 @@ with open('birth_weight.csv', "w", newline='') as f:
     f.close()
 with open('birth_weight.csv', "a+", newline='') as f:
     writer = csv.writer(f)
+    datas = []
     for i in rss:
-        temp = ["0"]*165
+        temp = [0]*165
         for ikey,ivalue in i.items():
             index = keys.index(ikey)
+            sum = 0
             if index == -1:
                 pass
             else:
-
-                temp[index] = ivalue
+                for value in ivalue:
+                    sum = sum + int(value)
+                temp[index] = sum/len(ivalue)
         writer.writerow(temp)
+        datas.append(temp)
+    print(datas)
+    scio.savemat('csv.mat', mdict={"csv": datas})
     f.close()
